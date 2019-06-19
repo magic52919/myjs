c1b0caf8216c41d8ca86f29096deae046f8b46f1
magic <magic@gmail.com>
2019-06-19 18:01:08 +0800

    js

---
+++ demo.js
@@ -0,0 +1,16 @@
+let var1;
+//document.write 對文件輸出
+document.write(var1 + "<br>");
+var1 =123;
+document.write(typeof(var1)+ "<br>");
+var1 =true;
+document.write(typeof(var1)+ "<br>");
+var1 ="我";
+document.write(typeof(var1)+ "<br>");
+
+//常數const(變數通常全大寫)
+//一定要給值,且不能變更
+const name='magic';
+document.write("myname is"+ name+  "<br>");
+document.write(`myname is ${name}`);
+
